9c5ddf2b6eda93e16dbb075d341187e1540cff5d
Nazar Kulyk <nazar.kulyk@ibsolution.de>
2019-03-12 13:07:53 +0200

    cleanup first ruleset

---
+++ README.md
@@ -0,0 +1,7 @@
+# ESLint Base Rules
+
+## Add Base ruleset to project
+
+```
+npm install --save-dev git+ssh://bitbucket.org:ibsolution_de/eslint-config-ibsolution-base.git eslint eslint-plugin-import
+```
--- rules/best-practices.js
+++ rules/best-practices.js
@@ -2,10 +2,7 @@ module.exports = {
     rules: {
         'accessor-pairs': 'off',
         indent: [2, 4, { SwitchCase: 1 }],
-        quotes: [1, 'single', { avoidEscape: true }],
-        'jsx-quotes': [2, 'prefer-single'],
         'linebreak-style': [2, 'unix'],
-        semi: [2, 'always'],
         strict: [2, 'function'],
         'no-multiple-empty-lines': [2, { max: 1 }],
         'max-len': ['error', { code: 120 }]
